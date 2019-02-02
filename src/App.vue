fbfc837e685ac24050d1b43a41955e3440fbcc54
AliNisarAhmed <ali.nisar87@gmail.com>
2019-02-02 19:03:30 +0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <div class="nav">
+      <h1 class="title">Wikipedia Viewer</h1>
+    </div>
+    <div class="center">
+      <p>click <a href="https://en.wikipedia.org/wiki/Special:Random" target="_blank" class="random">here</a> to read a random article.</p>
+      <form :class="{active: active}" v-on:submit.prevent="handleFormSubmit">
+        <input type="text" v-on:focus="toggleActive" v-on:blur="toggleActive" v-model="searchField">
+      </form>
+      <p>Click the icon to search</p>
+    </div>
+    <div class="results">
+      <div v-if="isLoading" class="loading-icon-parent">
+        <div class="loading-icon"></div>
+      </div>
+      <div v-else-if="!isLoading" class="result-list">
+        <Results v-for="result in results" :key="result.pageid" :resultTitle="result.title"></Results>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Axios from 'axios';
+import Results from './components/Results';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Results: Results
+  },
+  data () {
+    return {
+      active: false,
+      searchField: "",
+      isLoading: false,
+      results: null,
+    }
+  },
+  methods: {
+    toggleActive () {
+      this.active = !this.active;
+      if (this.searchField.length > 0) this.searchField = "";
+    },
+    async handleFormSubmit () {
+      this.results = null;
+      let searchTerm = this.searchField;
+      this.searchField = "";
+      this.isLoading = true;
+      let res = await Axios.get(`https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=${encodeURI(searchTerm)}`);
+      this.isLoading = false;
+      console.log(res);
+      this.results = res.data.query.search;
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>{{ resultTitle }}</p>
+</template>
+
+<script>
+export default {
+  name: "Results",
+  props: ["resultTitle"]
+}
+</script>
+
+<style>
+
+</style>
